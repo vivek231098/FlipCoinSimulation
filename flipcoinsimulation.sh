0e5cdcd95c4df0604fe6bd326b4606d397cd2090
vivek231098 <menonvivek196@gmail.com>
2020-08-08 16:55:55 +0530

    Difference generated

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -2,15 +2,21 @@ Heads=0
 Tails=0
 while [[ $Heads -lt 21 && $Tails -lt 21 ]]
 do
- randomCheck=$((RANDOM%2))
-         if [ $randomCheck -eq 1 ]
-         then
-                 ((Heads++))
-         else
-                ((Tails++))
-         fi
+	randomCheck=$((RANDOM%2))
+	if [ $randomCheck -eq 1 ]
+	then
+		((Heads++))
+	else
+		((Tails++))
+	fi
 done
-if [ $randomCheck -eq 1 ]
+if [ $Heads -eq $Tails ]
+then
+echo " Tie is incurred "
+	while [ $(($Heads-$Tails)) -eq 0 ]
+	do
+	 	randomCheck=$((RANDOM%2))
+   	if [ $randomCheck -eq 1 ]
    	then
       	((Heads++))
    	else
@@ -27,6 +33,11 @@ if [ $randomCheck -eq 1 ]
       	fi
    	done
 	done
-
 echo "Heads: $Heads"
 echo "Tails : $Tails"
+elif [ $Heads -gt $Tails ]
+then
+  echo "Heads won by $(($Heads-$Tails)) times more than Tails"
+else
+  echo "Tails won by $(($Tails-$Heads)) times more than Heads"
+fi
