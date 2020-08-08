92eeecb21dd19d37418869442e51bc57a9517e21
vivek231098 <menonvivek196@gmail.com>
2020-08-08 16:40:20 +0530

    Multiple tosses generated

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -1,4 +1,3 @@
-
 randomCheck=$((RANDOM%2))
         if [ $randomCheck -eq 1 ]
         then
@@ -6,3 +5,20 @@ randomCheck=$((RANDOM%2))
         else
                 ((Tails++))
         fi
+if [ $randomCheck -eq 1 ]
+   	then
+      	((Heads++))
+   	else
+      	((Tails++))
+   	fi
+		while [[ $(($Heads-$Tails)) -eq 1 || $(($Tails-$Heads)) -eq 1 ]]
+   	do
+      	randomCheck=$((RANDOM%2))
+      	if [ $randomCheck -eq 1 ]
+      	then
+         	((Heads++))
+      	else
+         	((Tails++))
+      	fi
+   	done
+	done
